062c6352b50db717f9d3c79b6cc66c61204b41d6
Sylvrwolflol <46539700+Sylvrwolflol@users.noreply.github.com>
2023-09-17 10:35:37 -0400

    Add files via upload

---
+++ xivlauncher_physics_plugin-main/xivlauncher_physics_plugin-main/SamplePlugin/HighFpsPhysicsPlugin.cs
@@ -0,0 +1,75 @@
+﻿using Dalamud.Game;
+using Dalamud.Game.Command;
+using Dalamud.Plugin;
+
+namespace HighFpsPhysicsPlugin;
+
+
+public sealed class HighFpsPhysicsPlugin : IDalamudPlugin
+{
+    private const string CommandName = "/physics";
+
+    public HighFpsPhysicsPlugin(DalamudPluginInterface pluginInterface, Framework framework)
+    {
+        pluginInterface.Create<Service>();
+
+        Service.Settings = pluginInterface.GetPluginConfig() as Configuration ?? new Configuration();
+        Service.Settings.Initialize(pluginInterface);
+
+        Service.PhysicsModification = new Physics(framework);
+
+        Service.Commands.AddHandler(CommandName, new CommandInfo(OnCommand)
+        {
+            HelpMessage = "Open Configuration Window\n" +
+                          "/physics on - Enable Physics Change\n" +
+                          "/physics off - Disable Physics Change\n" +
+                          "/physics t - Toggle Physics Change"
+        });
+
+        Service.WindowSystem.AddWindow(new ConfigurationWindow());
+
+        Service.PluginInterface.UiBuilder.Draw += Service.WindowSystem.Draw;
+        Service.PluginInterface.UiBuilder.OpenConfigUi += () => Service.WindowSystem.Windows[0].IsOpen = true;
+    }
+
+    public string Name => "High FPS Physics Fix";
+
+    public void Dispose()
+    {
+        Service.Commands.RemoveHandler(CommandName);
+        Service.WindowSystem.RemoveAllWindows();
+
+        Service.PhysicsModification.Dispose();
+    }
+
+    private void OnCommand(string command, string args)
+    {
+        var configWindow = Service.WindowSystem.Windows[0];
+
+        switch (args)
+        {
+            case "":
+                configWindow.IsOpen = !configWindow.IsOpen;
+                break;
+
+            case "on":
+                Service.PhysicsModification.Enable();
+                break;
+
+            case "off":
+                Service.PhysicsModification.Disable();
+                break;
+
+            case "t":
+                if (Service.PhysicsModification.GetStatus())
+                {
+                    Service.PhysicsModification.Disable();
+                }
+                else
+                {
+                    Service.PhysicsModification.Enable();
+                }
+                break;
+        }
+    }
+}
---
+++ xivlauncher_physics_plugin-main/xivlauncher_physics_plugin-main/SamplePlugin/Service.cs
@@ -0,0 +1,17 @@
+﻿using Dalamud.Game.Command;
+using Dalamud.Game.Gui;
+using Dalamud.Interface.Windowing;
+using Dalamud.IoC;
+using Dalamud.Plugin;
+
+namespace HighFpsPhysicsPlugin;
+
+internal class Service
+{
+    public static Physics PhysicsModification = null!;
+    public static Configuration Settings = null!;
+    public static WindowSystem WindowSystem = new("HighFPSPhysics");
+    [PluginService] public static ChatGui Chat { get; private set; } = null!;
+    [PluginService] public static CommandManager Commands { get; private set; } = null!;
+    [PluginService] public static DalamudPluginInterface PluginInterface { get; private set; } = null!;
+}
